5103a85fabc8b0093527ed2d9ab2bac54755dc62
Seokyung <xuejd17@gmail.com>
2023-11-24 01:06:33 +0900

    🌱 update: 섹션별 파일 초기화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Menu from "./components/Menu";
+import Main from "./sections/Main";
+import AboutMe from "./sections/AboutMe";
+import Skills from "./sections/Skills";
+import Project from "./sections/Project";
+import Activity from "./sections/Activity";
+import Footer from "./components/Footer";
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Menu />
+			<Main />
+			<AboutMe />
+			<Skills />
+			<Project />
+			<Activity />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
