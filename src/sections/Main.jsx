7259d45ca909d088a967eaf5f9929a5b8faf4a90
Seokyung <xuejd17@gmail.com>
2023-12-04 00:02:05 +0900

    💄 design: Main 섹션 디자인 적용

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -4,7 +4,7 @@ import { SectionWrapper } from "../styles/SectionWrapper";
 
 function Main() {
 	return (
-		<SectionWrapper $background="#f7d794">
+		<SectionWrapper>
 			<Wrapper>
 				<Logo>
 					<img src={seokyung_logo} alt="seokyung_logo" />
@@ -12,7 +12,11 @@ function Main() {
 				<Introduction>
 					<h2>안녕하세요!</h2>
 					{/* <h3>Web Frontend Developer Seokyung Jee</h3> */}
-					<span>꾸준히 성장하는 개발자 지서경입니다.</span>
+					<span>
+						꾸준히 성장하는 개발자
+						<br />
+						<span id="name">지서경</span>입니다.
+					</span>
 				</Introduction>
 			</Wrapper>
 		</SectionWrapper>
@@ -20,32 +24,61 @@ function Main() {
 }
 
 const Wrapper = styled.div`
-	flex: 1;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	gap: 1rem;
+	gap: 3.5rem;
+	width: 100%;
+	height: 100%;
+	padding: 2.5rem 1.5rem;
+	background-color: #f8f8f8;
+	border-radius: 20px;
+	@media (max-width: 768px) {
+		flex-direction: column;
+		gap: 3rem;
+	}
 `;
 
 const Logo = styled.div`
-	flex: 1;
-	display: flex;
-	justify-content: flex-end;
+	overflow: hidden;
+	padding: 1rem;
+	background-color: #f4d6ae;
+	border-radius: 50%;
+	box-shadow: 0 0 12px 3px rgba(0, 0, 0, 0.25);
 	img {
-		width: 200px;
+		width: 100%;
+		max-width: 250px;
+	}
+	@media (max-width: 768px) {
+		flex: none;
 	}
 `;
 
 const Introduction = styled.div`
-	flex: 1;
-	/* h2 {
-		color: #fff;
-		font-size: 2rem;
+	h2,
+	span {
+		font-family: "Nanum Gothic", sans-serif;
+	}
+	h2 {
+		margin-bottom: 1.25rem;
+		color: #333;
+		font-size: 3rem;
+		font-weight: 800;
 	}
 	span {
-		color: #fff;
-		font-size: 1.25rem;
-	} */
+		font-size: 2rem;
+		font-weight: 700;
+		line-height: 2.75rem;
+		#name {
+			margin-right: 0.25rem;
+			color: darkorange;
+			font-size: 2.25rem;
+			font-weight: 800;
+		}
+	}
+	@media (max-width: 768px) {
+		flex: none;
+	}
 `;
 
 export default Main;
--- src/styles/SectionWrapper.jsx
+++ src/styles/SectionWrapper.jsx
@@ -8,5 +8,5 @@ export const SectionWrapper = styled.div`
 	width: 100%;
 	min-height: calc(100vh - 60px);
 	padding: 1.5rem 2rem;
-	background: ${(props) => props.$background || "#fff"};
+	background: ${(props) => props.$background || "#333"};
 `;
