965605c6a554cbfeda4486f19b2e4e7a1de99c26
Seokyung <xuejd17@gmail.com>
2023-12-03 23:16:34 +0900

    💄 design: 섹션 ui 설정

--- src/styles/SectionWrapper.jsx
+++ src/styles/SectionWrapper.jsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const SectionWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 	width: 100%;
 	min-height: calc(100vh - 60px);
 	padding: 1.5rem 2rem;
