58e3aae4ef8c75fee74574cb2b2718a91f5bf019
Seokyung <xuejd17@gmail.com>
2023-12-03 22:59:40 +0900

    💄 design: 섹션 최소 높이 설정

--- src/styles/SectionWrapper.jsx
+++ src/styles/SectionWrapper.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const SectionWrapper = styled.div`
 	width: 100%;
+	min-height: calc(100vh - 60px);
 	padding: 1.5rem 2rem;
 	background: ${(props) => props.$background || "#fff"};
 `;
