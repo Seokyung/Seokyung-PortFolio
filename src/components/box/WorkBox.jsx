e4afc6782747a60454d4252c60c275e37d2d1a6d
Seokyung <xuejd17@gmail.com>
2024-08-24 18:40:08 +0900

    💄 design: WorkExperience pc, 모바일 디자인 적용

---
+++ src/components/box/WorkBox.jsx
@@ -0,0 +1,153 @@
+import styled from "styled-components";
+import { ReactComponent as LinkIcon } from "../../assets/svg/link_icon.svg";
+
+const WorkBox = ({ item }) => {
+	return (
+		<Box key={item?.id}>
+			<CompanyBox>
+				<h2>{item?.companyName}</h2>
+				<span>{item?.companyInfo}</span>
+			</CompanyBox>
+			<Divider />
+			<ContentWrapper>
+				<RoleBox>
+					<span className="sector">{item?.sector}</span>
+					<span className="role">{item?.role}</span>
+					<span className="period">{item?.period}</span>
+				</RoleBox>
+				<DescriptionBox>
+					<ContentBox>
+						<h4>주요 업무</h4>
+						<ul>
+							{item?.works.map((el, idx) => {
+								return <li key={idx}>{el}</li>;
+							})}
+						</ul>
+					</ContentBox>
+					<ContentBox>
+						<h4>링크</h4>
+						<ul>
+							{item?.links.map((el, idx) => {
+								return (
+									<li key={idx}>
+										<a href={el.url} target="_blank" rel="noreferrer">
+											{el.siteName} <LinkIcon />
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+					</ContentBox>
+				</DescriptionBox>
+			</ContentWrapper>
+		</Box>
+	);
+};
+
+const Box = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding: 1.25rem 1rem;
+	width: 100%;
+	background-color: var(--bg-color-light);
+	border-radius: 10px;
+`;
+
+const CompanyBox = styled.div`
+	h2 {
+		margin-bottom: 0.5rem;
+		color: var(--font-color-primary);
+		font-family: var(--font-family-kor-title);
+		font-size: 2rem;
+		letter-spacing: 0.5px;
+		line-height: 2.25rem;
+	}
+	span {
+		color: var(--font-color-secondary);
+		font-size: 1rem;
+		font-weight: 500;
+		line-height: 1.25rem;
+		letter-spacing: -0.5px;
+		word-spacing: 1.25px;
+	}
+`;
+
+const Divider = styled.hr`
+	width: 100%;
+	height: 1px;
+	margin: 1rem 0;
+`;
+
+const ContentWrapper = styled.div`
+	display: flex;
+	gap: 1.5rem;
+	padding: 0.5rem;
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
+`;
+
+const RoleBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.75rem;
+	span {
+		color: var(--font-color-secondary);
+		font-family: var(--font-family-eng);
+		font-size: 1rem;
+		letter-spacing: -0.25px;
+		line-height: 1.25rem;
+	}
+	.sector,
+	.role,
+	.period {
+	}
+`;
+
+const DescriptionBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.25rem;
+`;
+
+const ContentBox = styled.div`
+	border-radius: 10px;
+	h4 {
+		color: var(--font-color-primary);
+		margin-bottom: 0.5rem;
+		font-size: 1rem;
+		font-weight: 600;
+		letter-spacing: -1.5px;
+		word-spacing: -1.25px;
+	}
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		padding-left: 1.125rem;
+		list-style-type: "- ";
+		li {
+			color: var(--font-color-secondary);
+			font-size: 0.9rem;
+			letter-spacing: -0.5px;
+			word-spacing: 1px;
+			line-height: 1.375rem;
+			a {
+				svg {
+					color: var(--color-orange);
+					width: 14px;
+					height: auto;
+				}
+				&:visited {
+					color: inherit;
+				}
+				&:hover,
+				&:hover svg {
+					color: var(--color-orange);
+				}
+			}
+		}
+	}
+`;
+
+export default WorkBox;
--- src/sections/WorkExperience.jsx
+++ src/sections/WorkExperience.jsx
@@ -3,18 +3,21 @@ import {
 	SectionWrapperInner,
 	SectionWrapperOuter,
 } from "../styles/SectionWrapper";
-import ProjectBox from "../components/box/ProjectBox";
 import { skg_work_experiences } from "../assets/skg_infos";
+import WorkBox from "../components/box/WorkBox";
 
 const WorkExperience = () => {
 	return (
-		<SectionWrapperOuter id="work-experiences">
+		<SectionWrapperOuter
+			id="work-experiences"
+			$background="var(--color-dark-brown)"
+		>
 			<SectionWrapperInner>
 				<Wrapper>
 					<h1>Work Experiences</h1>
 					<div>
 						{skg_work_experiences.map((item) => {
-							return <ProjectBox key={item.id} project={item} />;
+							return <WorkBox key={item.id} item={item} />;
 						})}
 					</div>
 				</Wrapper>
@@ -25,16 +28,14 @@ const WorkExperience = () => {
 
 const Wrapper = styled.div`
 	width: 100%;
-	/* max-width: 1000px; */
-	margin: 1rem 0;
+	margin: 2rem 0;
 	h1 {
-		margin-bottom: 1rem;
-		padding: 0.5rem;
-		/* color: #cc6c1c; */
+		margin: 0 0 0.25rem 0.5rem;
 		color: var(--font-color-primary);
 		font-family: var(--font-family-eng-title);
-		font-size: var(--font-size-title-lg);
-		line-height: 2.75rem;
+		font-size: 2rem;
+		letter-spacing: -1.5px;
+		line-height: 2.5rem;
 	}
 `;
 
